f5c8f19e256298801c8b0cf5b67782bfcda9b81a
Lucie Augé <lucieauge@MacBook-Pro-de-Lucie.local>
2019-08-16 17:19:28 +0200

    Ajout programme principal pour le jeu dans master

---
+++ PlusOuMoins.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int nombreEntre = 0; nombreMystere = 0;
+
+	while (nombreMystere => 0 && =< 100)
+	{
+		printf("Entrez le nombre mystère");
+		scanf("%d", &nombreMystere);
+	}
+
+	while nombreEntre != nombreMystere
+	{
+		printf("Quel est le nombre mystere ?");
+		scanf("%d", &nombreEntre)
+	if (nombreEntre < nombreMystere)
+	{
+		printf("c'est plus !\n");
+	}
+	else if (nombreEntre > nombreMystere)
+	{
+		printf("C'est moins !\n");
+	}
+	else 
+	{
+		printf("Bravo, vous avez trouvé le nombre mystere !\n");
+	}
+
+	}
